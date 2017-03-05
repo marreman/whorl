a9e6ff0b2149cf8f18b9f354151a9610aa5f919f
Martin Liberg <martin.liberg@izettle.com>
2017-03-05 21:50:49 +0100

    Add log middleware and some jsdoc

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+const compose = require("compose-function")
+
+
+/**
+ * Allows an update function to transform the
+ * model based on the message sent through Hjelm.
+ */
 function send(hjelm) {
   return hjelm.program.model =
     hjelm.program.update(hjelm.message, hjelm.program.model)
 }
 
+
+/**
+ * Logs the Hjelm instance everytime a message is run.
+ */
+function log(hjelm) {
+  console.log(hjelm)
+  return hjelm
+}
+
+
+/** The Hjelm class */
 class Hjelm {
-  constructor(program) {
+
+  /**
+   * Creates a Hjelm instance
+   */
+  constructor(program, middlewares) {
     this.program = program
+    this.runMiddlewares = compose(...middlewares)
   }
 
   run(message) {
     return this.program.model =
-      send({ message, program: this.program })
+      this.runMiddlewares({ message, program: this.program })
   }
 }
 
+
 module.exports = program => {
-  return new Hjelm(program)
+  return new Hjelm(program, [send, log])
 }
